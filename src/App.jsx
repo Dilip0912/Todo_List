0b6d35066bb52938f90b4564c5d2465b2ec94f15
Dilip <dilipk1261@gmail.com>
2025-02-06 20:30:34 +0530

    Delete Todo

--- src/App.jsx
+++ src/App.jsx
@@ -26,6 +26,20 @@ function App() {
     setItem(newTodoItems);
   }
 
+    // console.log(item);
+    function handleOnDeleteClick(todoname,tododate){
+      // console.log(todoname,tododate);
+      // let removingElement={todoName:todoname,todoDate:tododate}
+      
+      // let index=item.findIndex(items=>items.todoName===removingElement.todoName && items.todoDate===removingElement.todoDate)
+      // console.log(index);
+      // let itemCopy=item;
+      // console.log(itemCopy);
+      // itemCopy.splice(index,1);
+      let newArr=item.filter(items=>items.todoName!==removingElement.todoName || items.todoDate!==removingElement.todoDate)
+      // console.log(newArr);
+      setItem(newArr);
+    }
 
   return (
     <center className="todo_container">
