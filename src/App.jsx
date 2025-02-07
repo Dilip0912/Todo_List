f7341b90cac9ded531f2cb35248be488cd26aabc
Dilip <dilipk1261@gmail.com>
2025-02-07 10:06:45 +0530

    No Task Component Added

--- src/App.jsx
+++ src/App.jsx
@@ -7,45 +7,42 @@ import "./App.css";
 import AppName from "./AppName";
 import AddTodo from "./AddTodo";
 import TodoItem from "./components/TodoItem";
+import NoTask from "./components/NoTask";
 
 function App() {
-  // let item = ;
-  let [item,setItem] = useState([
-    {
-      todoName: "Trip",
-      todoDate: "24/02/25",
-    },
-    {
-      todoName: "Ride",
-      todoDate: "12/03/25",
-    },
-  ]);
-  function handleOnClick(todoname,tododate){
-    let newTodoItems=[...item,{"todoName":todoname,"todoDate":tododate}]
+  // let item = [
+  //   {
+  //     todoName: "Trip",
+  //     todoDate: "24/02/25",
+  //   },
+  //   {
+  //     todoName: "Ride",
+  //     todoDate: "12/03/25",
+  //   },
+  // ];
+  let [item, setItem] = useState([]);
+  function handleOnClick(todoname, tododate) {
+    let newTodoItems = [...item, { todoName: todoname, todoDate: tododate }];
     // console.log(newTodoItems);
     setItem(newTodoItems);
   }
 
-    // console.log(item);
-    function handleOnDeleteClick(todoname,tododate){
-      // console.log(todoname,tododate);
-      // let removingElement={todoName:todoname,todoDate:tododate}
-      
-      // let index=item.findIndex(items=>items.todoName===removingElement.todoName && items.todoDate===removingElement.todoDate)
-      // console.log(index);
-      // let itemCopy=item;
-      // console.log(itemCopy);
-      // itemCopy.splice(index,1);
-      let newArr=item.filter(items=>items.todoName!==removingElement.todoName || items.todoDate!==removingElement.todoDate)
-      // console.log(newArr);
-      setItem(newArr);
-    }
+  let itemCopy = item;
+  function handleOnDeleteClick(todoname, tododate) {
+    
+
+    let newArr=item.filter(items=>items.todoName!==removingElement.todoName || items.todoDate!==removingElement.todoDate)
+    console.log(newArr);
+    setItem(newArr);
+    setItem(newArr);
+  }
 
   return (
     <center className="todo_container">
       <AppName />
-      <AddTodo handleClick={handleOnClick}/>
+      <AddTodo handleClick={handleOnClick} />
       <TodoItem item={item} handleDeleteClick={handleOnDeleteClick}></TodoItem>
+      {item.length===0?<NoTask></NoTask>:""}
     </center>
   );
 }
---
+++ src/components/NoTask.jsx
@@ -0,0 +1,5 @@
+function NoTask(item){
+    return <h1>No Pending Task</h1>
+}
+
+export default NoTask;
