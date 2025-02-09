e45028172b02a94da4d788b74f9b9526bb02c90a
Dilip <dilipk1261@gmail.com>
2025-02-09 23:52:19 +0530

    New Hooks Used

--- src/AddTodo.jsx
+++ src/AddTodo.jsx
@@ -1,30 +1,41 @@
-import { useState } from "react";
-function AddTodo({handleClick}) {
+import { useState,useRef } from "react";
+function AddTodo({ handleClick }) {
 
-  let [todoinput,setTodoInput]=useState();
-  let [tododate,setTodoDate]=useState();
+  let todoNameRef=useRef();
+  let todoDateRef=useRef();
+  // let [todoinput, setTodoInput] = useState();
+  // let [tododate, setTodoDate] = useState();
 
-  function handleInputChange(event){
-    setTodoInput(event.target.value)
-  }
-  function handleDateChange(event){
-    setTodoDate(event.target.value)
-
-  }
-  function handleAddButton(){
-    handleClick(todoinput,tododate);
+  // function handleInputChange(event) {
+  //   setTodoInput(event.target.value);
+  // }
+  // function handleDateChange(event) {
+  //   setTodoDate(event.target.value);
+  // }
+  function handleAddButton() {
+    let todoinput=todoNameRef.current.value;
+    let tododate=todoDateRef.current.value;
+    handleClick(todoinput, tododate);
   }
   return (
     <div className="container text-center">
       <div className="row my_row">
         <div className="col-6">
-          <input type="text" placeholder="Enter your task" onChange={handleInputChange} />
+          <input
+            type="text"
+            placeholder="Enter your task"
+            ref={todoNameRef}
+          />
         </div>
         <div className="col-4">
-          <input type="date"  onChange={handleDateChange} />
+          <input type="date"  ref={todoDateRef}  />
         </div>
         <div className="col-2">
-          <button type="button" className="btn btn-primary" onClick={handleAddButton}>
+          <button
+            type="button"
+            className="btn btn-primary"
+            onClick={handleAddButton}
+          >
             Add
           </button>
         </div>
